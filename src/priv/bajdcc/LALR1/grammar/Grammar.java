0ccc3bbadb2e28f586bd9010233588716549601d
bajdcc <bajdcc@foxmail.com>
2018-01-12 21:32:17 +0800

    FIX: Update code
    * Use `lambda` instead of `func`

--- src/priv/bajdcc/LALR1/grammar/Grammar.java
+++ src/priv/bajdcc/LALR1/grammar/Grammar.java
@@ -219,7 +219,7 @@ public class Grammar extends Semantic {
 				"doc_list -> @LITERAL[0] [@COMMA doc_list[1]]");
 		/* 函数主体 */
 		infer(handler.getSemanticHandler("func"),
-				"func -> (@FUNCTION[10]#func_clearargs# | @YIELD#func_clearargs#) [@LSQ doc_list[0]{lost_doc} @RSQ] (@ID[1]#predeclear_funcname#{lost_func_name} | @NOT[1]#predeclear_funcname#{lost_func_name}) @LPA{lost_lpa} [var_list[2]] @RPA{lost_rpa} (@PTR_OP{lost_func_body} exp[3]{lost_exp} | block[4]{lost_func_body})");
+                "func -> (@FUNCTION[10]#func_clearargs# | @YIELD#func_clearargs#) [@LSQ doc_list[0]{lost_doc} @RSQ] (@ID[1]#predeclear_funcname#{lost_func_name} | @NOT[1]#predeclear_funcname#{lost_func_name}) @LPA{lost_lpa} [var_list[2]] @RPA{lost_rpa} (@PTR_OP#do_enter_scope#{lost_func_body} exp[3]#do_leave_scope#{lost_exp} | block[4]{lost_func_body})");
 		/* 匿名主体 */
 		infer(handler.getSemanticHandler("lambda"),
 				"lambda -> @LAMBDA[1]#lambda# @LPA{lost_lpa} [var_list[2]] @RPA{lost_rpa} (@PTR_OP{lost_func_body} exp[3]{lost_exp} | block[4]{lost_func_body})");
